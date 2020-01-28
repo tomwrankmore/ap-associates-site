f110a408664dea8ddaa0b4b29fbf2bcde010d93e
Tom <tomwrankmore@gmail.com>
2020-01-28 13:40:07 +0000

    pretty much made the site

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import '../styles/styles.css'
 import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 
+new MobileMenu();
+
 if(module.hot) {
   module.hot.accept()
 }
 
-// hot modules means that webpack generates javascript file to memory rather than creating a file.
+// hot modules means that webpack generates javascript file to memory rather than creating a file...

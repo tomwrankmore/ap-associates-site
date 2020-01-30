b8324b19a53a774ef9f661625c44b886dfd4a1ab
Tom <tomwrankmore@gmail.com>
2020-01-30 13:02:09 +0000

    image

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,27 +3,6 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 
 new MobileMenu();
-let i = 0;
-let images = [];
-let time = 2000;
-
-images[0] = '/assets/images/home-slide/home-slide-0.png';
-images[1] = '/assets/images/home-slide/home-slide-1.png';
-images[2] = '/assets/images/home-slide/home-slide-2.png';
-
-function changeImg() {
-  document.slide.src = images[i];
-
-  if(i < images.length - 1) {
-     i++;
-  } else {
-    i = 0;
-  }
-
-  setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
 
 if(module.hot) {
   module.hot.accept()

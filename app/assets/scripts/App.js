33b9b2591c6fb99a09c241b2301d022a8fe5372d
Tom <tomwrankmore@gmail.com>
2020-02-14 09:32:51 +0000

    animations

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
 import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 new MobileMenu();
 
@@ -9,3 +11,17 @@ if (module.hot) {
 }
 
 // hot modules means that webpack generates javascript file to memory rather than creating a file...
+
+
+AOS.init({
+  easing: 'ease-in-out',
+  duration: 500,
+  delay: 0,
+  once: true,
+  mirror: false,
+  disable: 'mobile',
+  anchorPlacement: 'top-center',
+});
+// window.addEventListener('load', function () {
+//   AOS.refresh();
+// });
